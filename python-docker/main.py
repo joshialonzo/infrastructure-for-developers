080c354155242d93dcc75b081ba6a1d85bc767b1
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-03-20 19:16:53 -0600

    Showing all containers

--- python-docker/main.py
+++ python-docker/main.py
@@ -1,1 +1,12 @@
 import docker
+
+
+client = docker.from_env()
+
+containers_list = client.containers.list(all=True)
+
+images_list = client.images.list()
+
+print("Showing all containers...")
+for container in containers_list:
+    print(container.name)
