e77aa0dced0da8f3ea470dc552540f480097669a
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-03-20 19:17:19 -0600

    Showing all images

--- python-docker/main.py
+++ python-docker/main.py
@@ -10,3 +10,11 @@ images_list = client.images.list()
 print("Showing all containers...")
 for container in containers_list:
     print(container.name)
+
+print()
+
+print("Showing all images...")
+for image in images_list:
+    tags = image.tags
+    first_tag = tags[0] if tags else "no-tag"
+    print(first_tag)
