a98050c201278a5a59de2080427c10167194a750
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-06-16 19:17:31 -0600

    Networking: Server

--- networking/main.py
+++ networking/main.py
@@ -6,14 +6,28 @@ class NetworkDevice:
     name: str
 
 
+@dataclass
+class Server:
+    name: str
+
+
 class Laptop(NetworkDevice):
     pass
+
+
+class EmailServer(Server):
+    pass
+
 
 
 def main():
     laptop_1 = Laptop(name="Laptop 1")
     assert isinstance(laptop_1, Laptop)
     assert isinstance(laptop_1, NetworkDevice)
+
+    server_1 = EmailServer(name="Gmail")
+    assert isinstance(server_1, Server)
+    assert isinstance(server_1, EmailServer)
 
 
 if __name__ == "__main__":
