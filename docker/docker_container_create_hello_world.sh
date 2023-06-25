e9177f3baac07dbcabba2de599b976107842300a
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-06-24 20:22:02 -0600

    Docker: docker container create hello-world

---
+++ docker/docker_container_create_hello_world.sh
@@ -0,0 +1,8 @@
+# create a container but not run it
+
+# docker: main command
+# container: subcommand
+# create: action
+# hello-world: image name
+# linux: tag name
+docker container create hello-world:linux
