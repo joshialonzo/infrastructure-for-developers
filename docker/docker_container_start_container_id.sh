952858334a24367c6c7645e7c02a63385505628b
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-06-24 21:47:02 -0600

    Docker: docker container start --attach container_id

--- docker/docker_container_start_container_id.sh
+++ docker/docker_container_start_container_id.sh
@@ -5,3 +5,11 @@
 # start: action
 # container_id: value created at runtime
 docker container start container_id
+
+
+# docker: main command
+# container: subcommand
+# start: action
+# --attach: see the logs of the container
+# container_id: value created at runtime
+docker container start --attach container_id
