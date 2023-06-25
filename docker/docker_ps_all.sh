83760911b240082807c4d14cbb9144f72cfa32a6
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-06-24 21:55:41 -0600

    Docker: docker create and start with run

--- docker/docker_ps_all.sh
+++ docker/docker_ps_all.sh
@@ -1,3 +1,4 @@
 # containers that are running and stopped
 
+# docker ps -a
 docker ps --all
---
+++ docker/docker_run_hello_world_linux.sh
@@ -0,0 +1,8 @@
+# create a container but not run it
+
+# docker: main command
+# container: subcommand
+# create: action
+# hello-world: image name
+# linux: tag name
+docker run hello-world:linux
